7d2099ce80c2ecd12176e96d967481e9b43ddd7f
Isaiah Cuzzupe <upmatter@hotmail.com>
2020-11-30 15:23:24 +1100

    pods installed

---
+++ Pods/Target Support Files/Pods-1RMCal-1RMCalUITests/Pods-1RMCal-1RMCalUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_1RMCal_1RMCalUITests : NSObject
+@end
+@implementation PodsDummy_Pods_1RMCal_1RMCalUITests
+@end
---
+++ Pods/Target Support Files/Pods-1RMCalTests/Pods-1RMCalTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_1RMCalTests : NSObject
+@end
+@implementation PodsDummy_Pods_1RMCalTests
+@end

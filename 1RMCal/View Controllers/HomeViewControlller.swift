c6e21f1f92603a481cd96d86134b6d4dc4eee7d8
Isaiah Cuzzupe <upmatter@hotmail.com>
2020-12-13 10:58:27 +1100

    segues set up

---
+++ 1RMCal/View Controllers/HomeViewControlller.swift
@@ -0,0 +1,42 @@
+//
+
+//
+//  ViewController.swift
+//  1RMCal
+//
+//  Created by Isaiah Cuzzupe on 30/11/20.
+//  Copyright © 2020 Isaiah Cuzzupe. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    //Segue Buttons
+
+    
+    //Button Actions
+    @IBAction func goToWorkouts(_ sender: Any) {
+    }
+    @IBAction func goToExercises(_ sender: Any) {
+    }
+    @IBAction func goToCalculator(_ sender: Any) {
+        performSegue(withIdentifier: "goToExercises", sender: self)
+    }
+    @IBAction func goToStats(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "goToExercises" {
+            guard let vc = segue.destination as? CalViewController else { return }
+        }
+    }
+
+
+}
+

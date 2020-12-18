34f5cd8d1f3fdf89ad7b7443729933a3f4d2e9e5
Isaiah Cuzzupe <upmatter@hotmail.com>
2020-12-18 20:58:43 +1100

    Model Strcutrue v.1

---
+++ 1RMCal/Model/Model.swift
@@ -0,0 +1,75 @@
+//
+//  Exercise.swift
+//  1RMCal
+//
+//  Created by Isaiah Cuzzupe on 18/12/20.
+//  Copyright © 2020 Isaiah Cuzzupe. All rights reserved.
+//
+
+import Foundation
+
+// Model Management
+
+// Managers all exercises
+class ExerciseManager {
+    var exercies : [Exercise]?
+}
+
+// A workout is a exercise manager with a set of exercises
+typealias Workout = ExerciseManager
+
+// Managers all workouts
+class WorkoutManager {
+    var workouts : [Workout]?
+}
+
+
+
+// Represents a Exercise (holds info for the exxercises name & all performences of the exxcercise)
+class Exercise {
+    // Name of exercise
+    var name : String
+    //Performences of Exercise
+    var instances : [ExerciseInstance]?
+    
+    init(name : String) {
+        self.name = name
+    }
+    
+}
+
+// Represents a single performence/instance of a exercise
+class ExerciseInstance {
+    // time exercise was performed
+    var date : Date
+    // Sets performed
+    var sets : [SetStat]
+    
+    init(sets : [SetStat]) {
+        self.sets = sets
+        self.date = Date()
+    }
+}
+
+
+
+// Represents a single set in a exercise instance
+class SetStat {
+    
+    var repCount : Int
+    var weight : Double
+    var units : Weight
+    
+    var oneRM : Double {
+        var val = self.weight * (1 + (Double(self.repCount)/30))
+        val = val.rounded()
+        return val
+    }
+    
+    init(repCount : Int, weight : Double, units : Weight) {
+        self.repCount = repCount
+        self.weight = weight
+        self.units = units
+    }
+}
+

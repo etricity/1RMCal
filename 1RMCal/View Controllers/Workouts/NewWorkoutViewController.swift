745c42e316a59eaccb7038b0580a45b4c75c285c
Isaiah Cuzzupe <upmatter@hotmail.com>
2020-12-23 17:05:34 +1100

    swapping branches

---
+++ 1RMCal/View Controllers/Workouts/NewWorkoutViewController.swift
@@ -0,0 +1,20 @@
+//
+//  NewWorkoutViewController.swift
+//  1RMCal
+//
+//  Created by Isaiah Cuzzupe on 23/12/20.
+//  Copyright © 2020 Isaiah Cuzzupe. All rights reserved.
+//
+
+import UIKit
+
+class NewWorkoutViewController: UIViewController {
+
+    @IBOutlet weak var exercisesTableView: UITableView!
+    @IBOutlet weak var workoutLayout: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
